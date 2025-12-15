e9b2653a5f4d5c2def448529f75b245c87dc7c1a
DakshC <dchau012@uottawa.ca>
2025-12-14 21:12:48 -0500

    feat: Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from arxiv-classifier!")
+import argparse
+import modal
 
+def run_cloud_inference(model_type: str):
+    if model_type == "distilbert":
+        print(f"🚀 Launching DistilBERT Inference on Cloud...")
+        print("Run this command: modal run src/inference/inference_distilbert.py")
+        
+    elif model_type == "llama":
+        print(f"🦙 Launching Llama-3 Inference on Cloud...")
+        print("Run this command: modal run src/inference/inference_lora.py")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True, choices=["distilbert", "llama"])
+    args = parser.parse_args()
+    
+    run_cloud_inference(args.model)
